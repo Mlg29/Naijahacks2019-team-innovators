a64b8689c34335e6bbdca1efe8a601786eab45c1
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-10 00:38:11 +0100

    setup mongoose

--- dist/app.js
+++ dist/app.js
@@ -5,28 +5,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
+const mongoose = require('mongoose');
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const index_1 = __importDefault(require("./routes/index"));
 const users_1 = __importDefault(require("./routes/users"));
 var app = express_1.default();
 // view engine setup
-app.set('views', path_1.default.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(morgan_1.default('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(cookie_parser_1.default());
-app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+//connect to Mongo DB.
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', onMongoConnected);
+mongoose.connection.on('disconnected', onMongoDisconnected);
+mongoose.connection.on('error', onMongoError);
+function onMongoConnected() {
+    debug("MongodDB Connected Successfully");
+}
+function onMongoDisconnected() {
+    debug("MongodDB Disconnected - %o", err);
+}
+function onMongoError() {
+    debug("MongodDB error", error);
+}
 app.use('/', index_1.default);
 app.use('/users', users_1.default);
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (_req, _res, next) {
     next(http_errors_1.default(404));
 });
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res, _next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
