630e4a3466594dfb2f12615c80e5122af94335ff
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-10 16:02:33 +0100

    created users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,17 @@
-import express,{Request, Response, NextFunction} from "express"
+import express from "express"
 var router = express.Router();
+import {getAllUser,getSingleUser,createUser,updateUser,deleteUser} from "../controllers/users";
+
+//
+router.post("/", createUser)
+        .get("/", getAllUser)
+        .get("/:id",getSingleUser)
+        .patch("/:id",updateUser)
+        .delete("/:id",deleteUser)
 
 /* GET users listing. */
-router.get('/', function(req:Request, res:Response, _next:NextFunction) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(_req:Request, res:Response, _next:NextFunction) {
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+export default router;
