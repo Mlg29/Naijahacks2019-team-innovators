640fea7e5adc2c6f3b768515c706cc4b2a8305bb
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-16 01:14:36 +0100

    added dashboard menu item

---
+++ client/src/common/menu.jsx
@@ -0,0 +1,41 @@
+export default {
+  admin: [
+    {
+      title: 'Dashboard',
+      link: '/dashboard',
+      icon: '/assets/img/tachometer-alt-solid.svg',
+    },
+    {
+      title: 'Users',
+      link: '/dashboard/users',
+      icon: '/assets/img/users.svg',
+    },
+    {
+      title: 'Van',
+      link: '/dashboard/van',
+      icon: '/assets/img/caravan.svg',
+    },
+    {
+      title: 'waste',
+      link: '/dashboard/waste',
+      icon: '/assets/img/recycle.svg',
+    },
+    {
+      title: 'Dispatcher',
+      link: '/dashboard/dispatch',
+      icon: '/assets/img/dispatch.svg',
+    },
+    {
+      title: 'My Profile',
+      link: '/dashboard/profile',
+      icon: '/assets/img/user.svg',
+    },
+    {
+      title: 'Logout',
+      link: '/dashboard/logout',
+      icon: '/assets/img/logout.svg',
+    },
+    
+  ],
+  
+};
