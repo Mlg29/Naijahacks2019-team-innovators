586b8f5508833100025243ab519f34556fe525ca
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-16 01:41:44 +0100

    added dashboard data

---
+++ client/src/component2/dashboard-item/dashboardData.js
@@ -0,0 +1,32 @@
+export const dashboardData = [
+  {
+    id: 1,
+    title: 'VAN',
+    description: 'details about van.',
+    img: './assets/img/truck.svg',
+  },
+  {
+    id: 2,
+    title: 'WASTE',
+    description: 'detail about waste.',
+    img: './assets/img/recycle.svg',
+    link: 'Search waste',
+    // arrow: './assets/img/right-arrow-big.png',
+  },
+  {
+    id: 3,
+    title: 'Dispatcher',
+    description: 'Get your nearest dispatcher.',
+    img: './assets/img/dispatch.svg',
+    link: 'Dispatch',
+    // arrow: './assets/img/right-arrow-big.png',
+  },
+  {
+    id: 4,
+    title: 'Update Profile',
+    description: 'Update your details.',
+    img: './assets/img/dash-img5-big.png',
+    link: 'Update profile',
+    arrow: './assets/img/right-arrow-big.png',
+  },
+];
