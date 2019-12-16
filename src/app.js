f2e20b0df57d1ebc51f2356f3e5d1fd2dea2c096
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-12-16 18:15:21 +0100

    make it better

--- src/app.js
+++ src/app.js
@@ -29,6 +29,16 @@ app.use("/users", userRouter);
 app.use("/wastes", wasteRouter);
 app.use("/vehicles", vehicleRouter);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 /**
  * Create HTTP server.
  */
